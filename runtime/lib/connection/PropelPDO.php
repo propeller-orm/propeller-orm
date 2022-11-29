b7b017e3724c3f426a9f1b29d6acb0a810611048
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-11-29 16:43:02 +0200

    Introduce a conditional PropelPDO class declaration to resolve PHP8 type conflict

---
+++ runtime/lib/connection/PropelPDO.php
@@ -0,0 +1,45 @@
+<?php
+
+if (version_compare(PHP_VERSION, '8.0.0') >= 0) {
+    /**
+     *  * PDO connection subclass that provides the basic fixes to PDO that are required by Propel.
+     *
+     * This class was designed to work around the limitation in PDO where attempting to begin
+     * a transaction when one has already been begun will trigger a PDOException.  Propel
+     * relies on the ability to create nested transactions, even if the underlying layer
+     * simply ignores these (because it doesn't support nested transactions).
+     *
+     * The changes that this class makes to the underlying API include the addition of the
+     * getNestedTransactionDepth() and isInTransaction() and the fact that beginTransaction()
+     * will no longer throw a PDOException (or trigger an error) if a transaction is already
+     * in-progress.
+     */
+    class PropelPDO extends BasePropelPDO
+    {
+        public function query($statement, $mode = PDO::ATTR_DEFAULT_FETCH_MODE, ...$fetch_mode_args)
+        {
+            return parent::executeQuery($statement, $mode, $fetch_mode_args);
+        }
+    }
+} else {
+    /**
+     *  * PDO connection subclass that provides the basic fixes to PDO that are required by Propel.
+     *
+     * This class was designed to work around the limitation in PDO where attempting to begin
+     * a transaction when one has already been begun will trigger a PDOException.  Propel
+     * relies on the ability to create nested transactions, even if the underlying layer
+     * simply ignores these (because it doesn't support nested transactions).
+     *
+     * The changes that this class makes to the underlying API include the addition of the
+     * getNestedTransactionDepth() and isInTransaction() and the fact that beginTransaction()
+     * will no longer throw a PDOException (or trigger an error) if a transaction is already
+     * in-progress.
+     */
+    class PropelPDO extends BasePropelPDO
+    {
+        public function query($statement, $mode = PDO::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = [])
+        {
+            return parent::executeQuery(...func_get_args());
+        }
+    }
+}
